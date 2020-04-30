b8d3bcdb19f53b73ca1b449e35671254a1126438
manjil01 <gautamamanjil@gmail.com>
2020-04-30 12:56:07 -0500

    :sparkles: Added :page_facing_up: form for the Sub-reddit

--- reddit-client/src/views/Subreddit.vue
+++ reddit-client/src/views/Subreddit.vue
@@ -1,5 +1,19 @@
 <template>
-<h1>{{$route.params.name}}</h1>
+  <section>
+    <h1>{{ $route.params.name }}</h1>
+    <form>
+      <b-field label="Title">
+        <b-input></b-input>
+      </b-field>
+      <b-field label="description">
+        <b-input type="textarea"></b-input>
+      </b-field>
+      <b-field label="URL">
+        <b-input></b-input>
+      </b-field>
+       <button class="button is-success">Add Post</button>
+      </form>
+    </section>
 </template>
 
 <script>
